37ff0ed3f10aced1f93da79c856ed988a337eeb8
kosmin <ksagwey.vlad@mail.ru>
2024-04-24 17:48:22 +0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// app.js 
+
 const express = require('express');
 const path = require('path');
 const mainRoute = require('./routes/main');
 const gamesRouter = require('./routes/games'); 
 const bodyParser = require('body-parser');
+const cors = require('./middlewares/cors');
 
 const PORT = 3000;
 const app = express();
 
 // Теперь клиент имеет доступ только к публичным файлам
 app.use(
+  cors,
   bodyParser.json(),
   express.static(path.join(__dirname, 'public')),
   mainRoute,
--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,33 @@
-     const gamesRouter = require("express").Router(); 
-     const { sendAllGames, deleteGame, addGameController } = require("../controllers/games");
-     const { getAllGames } = require("../middlewares/games");
+const gamesRouter = require("express").Router();
 
+const {
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  findGameById,
+  deleteGame
+} = require("../middlewares/games");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
 
-     gamesRouter.post("/games", getAllGames, addGameController);
-     gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-     gamesRouter.get("/games", getAllGames, sendAllGames)
-     
-     module.exports = gamesRouter;
-     
+gamesRouter.get("/games", getAllGames, sendAllGames);
+
+gamesRouter.post(
+  "/games",
+  getAllGames,
+  checkIsTitleInArray,
+  updateGamesArray,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+gamesRouter.delete(
+  "/games/:id",
+  getAllGames,
+  findGameById,
+  deleteGame,
+  updateGamesFile,
+  sendUpdatedGames
+);
+
+module.exports = gamesRouter;
